6fee073012dbaf54575b7d4f35733eea148884cd
Terts Diepraam <terts.diepraam@gmail.com>
2022-12-15 13:30:00 +0100

    deploy

--- web/main.js
+++ web/main.js
@@ -1,7 +1,12 @@
 console.log(document.location.href);
+console.log(import.meta.env.BASE_URL);
 for (const el of document.querySelectorAll("a")) {
+    console.log(el.href);
+    // Remove trailing /
+    el.href = el.origin + import.meta.env.BASE_URL.slice(0, -1) + el.pathname;
     console.log(el.href);
     if (el.href === document.location.href) {
         el.classList.add("current");
     }
 }
+
